5522daef1e39a644cd0b521eb26921ac6aa4e2af
Oliver <oliver.vukovic@quantoxtechnology.com>
2021-10-12 16:28:54 +0200

    new short js code

--- main.js
+++ main.js
@@ -1,3 +1,70 @@
+// -----  calculate  -----
+
+function calculate(num){
+    
+    var result = document.getElementById("inputbox");
+    result.value += num;
+}
+
+function result(){
+    var result = document.getElementById("inputbox");
+   // result.value = eval(result.value);    
+
+    try{
+        result.value = eval(result.value)
+    }
+    catch(err){
+        alert("ERROR!");
+    }
+}
+
+function clr(){
+    var result = document.getElementById("inputbox");
+    result.value = " ";
+}
+
+function del() {
+    var result = document.getElementById("inputbox");
+    result.value = result.value.slice(0, -1);
+}
+
+
+
+// -----  change mode ----- 
+
+function changeMode(value)
+{
+    var color = document.body.style.backgroundColor;
+    switch(value) {
+        case 'mode-1':
+            color = "mode-1";
+        break;
+        case 'mode-2':
+            color = "mode-2";
+        break;
+        case 'mode-3':
+            color = "mode-3";
+        break;
+    }
+    document.body.classList = color;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*   ------------------------------------------------------------
+
 class Calculator {
     constructor(previousTextElement, currentTextElement) {
         this.previousTextElement = previousTextElement
@@ -123,3 +190,4 @@ deleteButton.addEventListener('click', button => {
     calculator.delete()
     calculator.updateDisplay()
 })
+*/
